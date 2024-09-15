150f8e789ade38c3625bce1bd2f6c9caf6090613
Max Wilson <m@qxotk.ai>
2024-09-15 10:47:30 +0100

    adjusted style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,57 @@
 import type { Component } from "solid-js";
 import { IconTypes } from "solid-icons";
+import { animate } from "motion";
+import { createSignal, JSX } from "solid-js";
 
-const ITEMS = ["Item 1", "Item 2", "Item 3"];
+import "./dropdown.css";
 
 type Option = {
   name: string;
-  icon?: string;
+  icon?: IconTypes;
+  onClick: (e: MouseEvent) => void;
 };
 
-const Dropdown: Component = () => {
-  /*
-  - 
-  */
-  return <></>;
+interface DropdownProps {
+  style: JSX.CSSProperties;
+  options: Option[];
+}
+
+const Dropdown: Component<DropdownProps> = ({ options, style }) => {
+  const [hoveredItem, setHoveredItem] = createSignal<string | null>(null);
+
+  const handleMouseEnter = (name: string) => {
+    setHoveredItem(name);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+
+  const handleClick = (option: Option) => {
+    option.onClick(new MouseEvent("click"));
+  };
+
+  return (
+    <div style={style} class="dropdown">
+      {options.map((option) => (
+        <div
+          // key={option.name}
+          class="dropdown-option"
+          onClick={() => handleClick(option)}
+          onMouseEnter={() => handleMouseEnter(option.name)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div class="icon">
+            {option.icon ? <option.icon /> : option.name.charAt(0)}
+          </div>
+          <span class="name">{option.name}</span>
+          {hoveredItem() === option.name && (
+            <div class="tooltip">{option.name}</div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default App;
+export default Dropdown;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,36 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import App from './App';
+import Dropdown from "./App";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
-render(() => <App />, root!);
+import { BiLogosUnity } from "solid-icons/bi";
+
+const options = [
+  {
+    name: "Option 1",
+    icon: BiLogosUnity,
+    onClick: (e: MouseEvent) => console.log("Option 1 clicked"),
+  },
+  {
+    name: "Two",
+    onClick: (e: MouseEvent) => console.log("Option 2 clicked"),
+  },
+];
+
+render(
+  () => (
+    <Dropdown
+      options={options}
+      style={{ left: "100px", top: "100px", width: "100px", height: "100px" }}
+    />
+  ),
+  root!
+);
